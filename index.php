2e2f040dffdc2546701744b35a824feeccc3ac88
yimemories <94780705+adzka-sfr@users.noreply.github.com>
2025-08-26 12:37:35 +0700

    upload awal

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+require_once $_SERVER['DOCUMENT_ROOT'] . '/cashier/config/connect.php'; // local
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/config/connect.php'; // hosting
+$user = require_once base_path('config/check_cookie.php');
+// Redirect to the apps if the user is authenticated
+echo "<script>window.location='" . base_url('apps') . "';</script>";
